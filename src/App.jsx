b4c56e03b931f9d561a7499c94d3f4967d1d843b
Serge Gatezh <2880401+gatezh@users.noreply.github.com>
2025-09-02 19:01:53 +0000

    Add basic Status component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import Header from './components/Header'
+import Header from './components/Header';
+import Status from './components/Status';
 import './App.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Header />
+      <Status />
     </>
   )
 }
---
+++ src/components/Status.jsx
@@ -0,0 +1,8 @@
+export default function Status() {
+  return (
+    <section className="status">
+      <h2>You win!</h2>
+      <p>Well done! 🎉🎉🎉</p>
+    </section>
+  )
+}
